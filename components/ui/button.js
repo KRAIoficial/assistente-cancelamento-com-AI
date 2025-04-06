af66a86659cc7e1d413f93f6203872f91c579365
KRAIoficial <contatokraioficial@gmail.com>
2025-04-06 09:09:18 -0300

    Primeiro commit do site Assistente de Cancelamento

--- components/ui/button.js
+++
@@ -1,4 +0,0 @@
-
-export function Button({ children, ...props }) {
-  return <button className="bg-blue-600 text-white rounded-xl p-3" {...props}>{children}</button>;
-}
--- components/ui/card.js
+++
@@ -1,8 +0,0 @@
-
-export function Card({ children }) {
-  return <div className="border shadow-md rounded-2xl">{children}</div>;
-}
-
-export function CardContent({ children, className }) {
-  return <div className={className}>{children}</div>;
-}
